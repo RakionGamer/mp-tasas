cfe7b790dfc8a0f0a5a6dc648b59eb83094378a9
RakionGamer <blackrvalera@gmail.com>
2026-06-10 14:25:37 -0400

    feat: Change bad identation in line 271-277

--- app/api/bot/route.js
+++ app/api/bot/route.js
@@ -272,9 +272,9 @@ export async function POST(req) {
         const esEnvioChile = /ENV[IÍ]O DESDE CHILE/i.test(texto);
         const esEnvioMexico = /ENV[IÍ]O DESDE M[EÉ]XICO/i.test(texto);
         const esEnvioVenezuela = /Env[ií]os? desde Venezuela/i.test(texto);
-        const esEnvioBrasil = /ENV[IÍ]OS DESDE BRASIL/i.test(texto);
-        const esEnvioPeru = /ENV[IÍ]OS DESDE PER[UÚ]/i.test(texto);
-        const esEnvioColombia = /ENV[IÍ]OS DESDE COLOMBIA/i.test(texto);
+        const esEnvioBrasil = /ENV[IÍ]O DESDE BRASIL/i.test(texto);
+        const esEnvioPeru = /ENV[IÍ]O DESDE PER[UÚ]/i.test(texto);
+        const esEnvioColombia = /ENV[IÍ]O DESDE COLOMBIA/i.test(texto);
 
         if (esCambios || esEnvioChile) {
           const processedImageUrlChile = await createImageWithRatesChile(
